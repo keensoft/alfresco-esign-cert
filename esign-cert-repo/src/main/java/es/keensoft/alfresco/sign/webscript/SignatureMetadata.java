d895f72bb66f8e546978f988a710871c2528b41e
Pedro Kravetz <p.v.kravetz@gmail.com>
2023-03-30 17:04:09 -0300

    Alterações para pegar novo jar

--- esign-cert-repo/src/main/java/es/keensoft/alfresco/sign/webscript/SignatureMetadata.java
+++ esign-cert-repo/src/main/java/es/keensoft/alfresco/sign/webscript/SignatureMetadata.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import org.alfresco.repo.content.MimetypeMap;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.extensions.webscripts.AbstractWebScript;
@@ -54,7 +54,7 @@ public class SignatureMetadata extends AbstractWebScript {
 			
 		} catch (Exception e) {
 			
-			log.error(ExceptionUtils.getFullStackTrace(e));
+			log.error(ExceptionUtils.getRootCauseStackTrace(e));
 			throw new WebScriptException(e.getMessage(), e);
 			
 		}
--- esign-cert-repo/src/main/java/es/keensoft/alfresco/sign/webscript/SignatureParams.java
+++ esign-cert-repo/src/main/java/es/keensoft/alfresco/sign/webscript/SignatureParams.java
@@ -3,7 +3,7 @@ package es.keensoft.alfresco.sign.webscript;
 import java.io.IOException;
 
 import org.alfresco.repo.content.MimetypeMap;
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.extensions.webscripts.AbstractWebScript;
